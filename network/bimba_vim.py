449c0170f80a613ede33308def2c991bbcbd6e2f
azam shirali <ashir018@quartz.cs.fiu.edu>
2025-11-20 19:29:16 -0500

    Add code and update documentation

---
+++ network/bimba_vim.py
@@ -0,0 +1,503 @@
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+import torch
+import torch.nn as nn
+from functools import partial
+from torch import Tensor
+from typing import Optional
+from timm.models.layers import trunc_normal_
+from timm.models.layers import DropPath, to_2tuple
+import math
+from utils.mamba_simple_xai import Mamba
+from utils.rope import *
+import random
+
+try:
+    from utils.triton.layernorm import RMSNorm, layer_norm_fn, rms_norm_fn
+except ImportError:
+    RMSNorm, layer_norm_fn, rms_norm_fn = None, None, None
+
+
+import ml_collections
+def get_ml_config(params):
+    """Returns the ViT configuration."""
+    config = ml_collections.ConfigDict()
+    config.patches = ml_collections.ConfigDict({'size': (params['patch_size'], params['patch_size'])})
+    config.hidden_size = params['hidden_size']
+    config.transformer = ml_collections.ConfigDict()
+    config.transformer.dropout_rate = params['dropout']
+    config.classifier = 'token'
+    config.representation_size = None
+    return config
+
+
+class PointFeatureEncoder(nn.Module):
+    def __init__(self, residue_dim=21, ss_dim=8, out_dim=16):
+        super().__init__()
+        self.scalar_fc = nn.Linear(4, out_dim)
+        self.vector_fc = nn.Linear(3, out_dim)
+        self.polarity_fc = nn.Linear(4, out_dim)
+        self.residue_fc = nn.Linear(residue_dim, out_dim)
+        self.ss_fc = nn.Linear(ss_dim, out_dim)
+        self.af = nn.GELU()
+
+        concat_dim = 5 * out_dim
+        self.last_fc = nn.Linear(concat_dim, 128)
+        self.af_combine = nn.GELU()
+
+    def forward(self, x):
+        scalar = x[:, :4]
+        vector = x[:, 4:7]
+        polarity = x[:, 7:11]
+        residue_oh = x[:, 11:32]
+        ss_oh = x[:, 32:]
+        scalar_feat = self.scalar_fc(scalar)
+        vector_feat = self.vector_fc(vector)
+        polarity_feat = self.polarity_fc(polarity)
+        residue_feat = self.residue_fc(residue_oh)
+        ss_feat = self.ss_fc(ss_oh)
+        concat_all = torch.cat([scalar_feat, vector_feat, polarity_feat, residue_feat, ss_feat], dim=-1)
+        ext_feats = self.af_combine(self.last_fc(concat_all))
+        return ext_feats
+
+
+class PatchEmbed(nn.Module):
+    """ 2D Image to Patch Embedding
+    """
+    def __init__(self, img_size=224, patch_size=16, stride=16, in_chans=3, embed_dim=128, norm_layer=None, flatten=True):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.grid_size = ((img_size[0] - patch_size[0]) // stride + 1, (img_size[1] - patch_size[1]) // stride + 1)
+        self.num_patches = self.grid_size[0] * self.grid_size[1]
+        self.flatten = flatten
+
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=stride)
+        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        x = self.proj(x)
+        if self.flatten:
+            x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
+        x = self.norm(x)
+        return x
+    
+  
+
+class Block(nn.Module):
+    def __init__(
+        self, dim, mixer_cls, norm_cls=nn.LayerNorm, fused_add_norm=False, residual_in_fp32=False,drop_path=0.,
+    ):
+        """
+        Simple block wrapping a mixer class with LayerNorm/RMSNorm and residual connection"
+
+        This Block has a slightly different structure compared to a regular
+        prenorm Transformer block.
+        The standard block is: LN -> MHA/MLP -> Add.
+        [Ref: https://arxiv.org/abs/2002.04745]
+        Here we have: Add -> LN -> Mixer, returning both
+        the hidden_states (output of the mixer) and the residual.
+        This is purely for performance reasons, as we can fuse add and LayerNorm.
+        The residual needs to be provided (except for the very first block).
+        """
+        super().__init__()
+        self.residual_in_fp32 = residual_in_fp32
+        self.fused_add_norm = fused_add_norm
+        self.mixer = mixer_cls(dim)
+        self.norm = norm_cls(dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        if self.fused_add_norm:
+            assert RMSNorm is not None, "RMSNorm import fails"
+            assert isinstance(
+                self.norm, (nn.LayerNorm, RMSNorm)
+            ), "Only LayerNorm and RMSNorm are supported for fused_add_norm"
+
+    def forward(
+        self, hidden_states: Tensor, residual: Optional[Tensor] = None, inference_params=None
+    ):
+        r"""Pass the input through the encoder layer.
+
+        Args:
+            hidden_states: the sequence to the encoder layer (required).
+            residual: hidden_states = Mixer(LN(residual))
+        """
+        if not self.fused_add_norm:
+            if residual is None:
+                residual = hidden_states
+            else:
+                residual = residual + self.drop_path(hidden_states)
+            
+            hidden_states = self.norm(residual.to(dtype=self.norm.weight.dtype))
+            if self.residual_in_fp32:
+                residual = residual.to(torch.float32)
+        else:
+            fused_add_norm_fn = rms_norm_fn if isinstance(self.norm, RMSNorm) else layer_norm_fn
+            if residual is None:
+                hidden_states, residual = fused_add_norm_fn(
+                    hidden_states,
+                    self.norm.weight,
+                    self.norm.bias,
+                    residual=residual,
+                    prenorm=True,
+                    residual_in_fp32=self.residual_in_fp32,
+                    eps=self.norm.eps,
+                )
+            else:
+                hidden_states, residual = fused_add_norm_fn(
+                    self.drop_path(hidden_states),
+                    self.norm.weight,
+                    self.norm.bias,
+                    residual=residual,
+                    prenorm=True,
+                    residual_in_fp32=self.residual_in_fp32,
+                    eps=self.norm.eps,
+                )    
+        hidden_states = self.mixer(hidden_states, inference_params=inference_params)
+        return hidden_states, residual
+
+    def allocate_inference_cache(self, batch_size, max_seqlen, dtype=None, **kwargs):
+        return self.mixer.allocate_inference_cache(batch_size, max_seqlen, dtype=dtype, **kwargs)
+
+
+def create_block(
+    d_model,
+    d_state=8,
+    ssm_cfg=None,
+    norm_epsilon=1e-5,
+    drop_path=0.,
+    rms_norm=False,
+    residual_in_fp32=False,
+    fused_add_norm=False,
+    layer_idx=None,
+    device=None,
+    dtype=None,
+    if_bimamba=False,
+    bimamba_type="none",
+    if_divide_out=False,
+    init_layer_scale=None,
+):
+    if if_bimamba:
+        bimamba_type = "v1"
+    if ssm_cfg is None:
+        ssm_cfg = {}
+    factory_kwargs = {"device": device, "dtype": dtype}
+    mixer_cls = partial(Mamba, d_state=d_state, use_fast_path=True, bimamba_type="v2", layer_idx=layer_idx, **ssm_cfg, **factory_kwargs)
+
+    norm_cls = partial(
+        nn.LayerNorm if not rms_norm else RMSNorm, eps=norm_epsilon, **factory_kwargs
+    )
+    block = Block(
+        d_model,
+        mixer_cls,
+        norm_cls=norm_cls,
+        drop_path=drop_path,
+        fused_add_norm=fused_add_norm,
+        residual_in_fp32=residual_in_fp32,
+    )
+    block.layer_idx = layer_idx
+    return block
+
+# https://github.com/huggingface/transformers/blob/c28d04e9e252a1a099944e325685f14d242ecdcd/src/transformers/models/gpt2/modeling_gpt2.py#L454
+def _init_weights(
+    module,
+    n_layer,
+    initializer_range=0.02, 
+    rescale_prenorm_residual=True,
+    n_residuals_per_layer=1, 
+):
+    if isinstance(module, nn.Linear):
+        if module.bias is not None:
+            if not getattr(module.bias, "_no_reinit", False):
+                nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.Embedding):
+        nn.init.normal_(module.weight, std=initializer_range)
+
+    if rescale_prenorm_residual:
+        for name, p in module.named_parameters():
+            if name in ["out_proj.weight", "fc2.weight"]:
+                nn.init.kaiming_uniform_(p, a=math.sqrt(5))
+                with torch.no_grad():
+                    p /= math.sqrt(n_residuals_per_layer * n_layer)
+
+def segm_init_weights(m):
+    if isinstance(m, nn.Linear):
+        trunc_normal_(m.weight, std=0.02)
+        if isinstance(m, nn.Linear) and m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.LayerNorm):
+        nn.init.constant_(m.bias, 0)
+        nn.init.constant_(m.weight, 1.0)
+
+
+class VisionMambaWithFeatures(nn.Module):
+    def __init__(self,
+                 config=None,
+                 img_size=18, 
+                 patch_size=3, 
+                 stride=3,
+                 depth=8, 
+                 embed_dim=128,
+                 d_state=8, 
+                 channels=5, 
+                 num_classes=2,
+                 ssm_cfg=None, 
+                 drop_rate=0.,
+                 drop_path_rate=0.1,
+                 norm_epsilon: float = 1e-5, 
+                 rms_norm: bool = True, 
+                 initializer_cfg=None,
+                 fused_add_norm=True,
+                 residual_in_fp32=True,
+                 device=None,
+                 dtype=None,
+                 ft_seq_len=None,
+                 pt_hw_seq_len=14,
+                 if_bidirectional=True,
+                 final_pool_type='none',
+                 if_abs_pos_embed=True,
+                 if_rope=False,
+                 if_rope_residual=False,
+                 flip_img_sequences_ratio=-1.,
+                 if_bimamba=False,
+                 bimamba_type="v2",
+                 if_cls_token=True,
+                 if_divide_out=True,
+                 init_layer_scale=None,
+                 use_double_cls_token=False,
+                 use_middle_cls_token=True,
+                 **kwargs):
+        self.config = config
+        factory_kwargs = {"device": device, "dtype": dtype}
+        kwargs.update(factory_kwargs) 
+        super().__init__()
+        self.residual_in_fp32 = residual_in_fp32
+        self.fused_add_norm = fused_add_norm
+        self.if_bidirectional = if_bidirectional
+        self.final_pool_type = final_pool_type
+        self.if_abs_pos_embed = if_abs_pos_embed
+        self.if_rope = if_rope
+        self.if_rope_residual = if_rope_residual
+        self.flip_img_sequences_ratio = flip_img_sequences_ratio
+        self.if_cls_token = if_cls_token
+        self.use_double_cls_token = use_double_cls_token
+        self.use_middle_cls_token = use_middle_cls_token
+        self.num_tokens = 1 if if_cls_token else 0
+        
+        self.cls_fc = nn.Linear(config.hidden_size + 128, 1)
+        
+        self.extra_feats_encoder = PointFeatureEncoder()
+        self.layernorm = nn.LayerNorm(config.hidden_size + 128)
+        self.combine_nn = nn.Linear(config.hidden_size+128, config.hidden_size)
+        self.af_combine = nn.GELU()
+        self.fc1_embed = nn.Linear(config.hidden_size, 64)
+        self.fc2_embed = nn.Linear(64, 32)
+
+        self.num_classes = num_classes
+        self.d_model = self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+
+        self.patch_embed = PatchEmbed(
+            img_size=img_size, patch_size=patch_size, stride=stride, in_chans=channels, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        if if_cls_token:
+            if use_double_cls_token:
+                self.cls_token_head = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
+                self.cls_token_tail = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
+                self.num_tokens = 2
+            else:
+                self.cls_token = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
+                
+        if if_abs_pos_embed:
+            self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + self.num_tokens, self.embed_dim))
+            self.pos_drop = nn.Dropout(p=drop_rate)
+
+        if if_rope:
+            half_head_dim = embed_dim // 2
+            hw_seq_len = img_size // patch_size
+            self.rope = VisionRotaryEmbeddingFast(
+                dim=half_head_dim,
+                pt_seq_len=pt_hw_seq_len,
+                ft_seq_len=hw_seq_len
+            )
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  
+        inter_dpr = [0.0] + dpr
+        self.drop_path = DropPath(drop_path_rate) if drop_path_rate > 0. else nn.Identity()
+
+        self.layers = nn.ModuleList(
+            [
+                create_block(
+                    embed_dim,
+                    d_state=d_state,
+                    ssm_cfg=ssm_cfg,
+                    norm_epsilon=norm_epsilon,
+                    rms_norm=rms_norm,
+                    residual_in_fp32=residual_in_fp32,
+                    fused_add_norm=fused_add_norm,
+                    layer_idx=i,
+                    if_bimamba=if_bimamba,
+                    bimamba_type=bimamba_type,
+                    drop_path=inter_dpr[i],
+                    if_divide_out=if_divide_out,
+                    init_layer_scale=init_layer_scale,
+                    **factory_kwargs,
+                )
+                for i in range(depth)
+            ]
+        )
+
+        self.norm_f = (nn.LayerNorm if not rms_norm else RMSNorm)(
+            embed_dim, eps=norm_epsilon, **factory_kwargs
+        )
+
+        self.patch_embed.apply(segm_init_weights)
+        self.head.apply(segm_init_weights)
+        if if_abs_pos_embed:
+            trunc_normal_(self.pos_embed, std=.02)
+        if if_cls_token:
+            if use_double_cls_token:
+                trunc_normal_(self.cls_token_head, std=.02)
+                trunc_normal_(self.cls_token_tail, std=.02)
+            else:
+                trunc_normal_(self.cls_token, std=.02)
+
+        self.apply(
+            partial(
+                _init_weights,
+                n_layer=depth,
+                **(initializer_cfg if initializer_cfg is not None else {}),
+            )
+        )       
+        
+    def allocate_inference_cache(self, batch_size, max_seqlen, dtype=None, **kwargs):
+        return {
+            i: layer.allocate_inference_cache(batch_size, max_seqlen, dtype=dtype, **kwargs)
+            for i, layer in enumerate(self.layers)
+        }
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {"pos_embed", "cls_token", "dist_token", "cls_token_head", "cls_token_tail"}
+
+    
+    def forward_features(self, x, inference_params=None):
+        # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+        x = self.patch_embed(x)
+        B, N, D = x.shape  # [B, num_patches, dim]
+        
+        if self.if_cls_token:
+            cls_token = self.cls_token.expand(B, 1, D)
+            
+            half = N // 2
+            x = torch.cat((x[:, :half], cls_token, x[:, half:]), dim=1)  # CLS in center
+            
+        if self.if_abs_pos_embed:
+            x = x + self.pos_embed
+            x = self.pos_drop(x)
+        
+        if_flip_img_sequences = False
+        if self.flip_img_sequences_ratio > 0 and (self.flip_img_sequences_ratio - random.random()) > 1e-5:
+            x = x.flip([1])
+            if_flip_img_sequences = True
+            
+        residual = None
+        hidden_states = x
+        if not self.if_bidirectional:
+            for layer in self.layers:
+                if if_flip_img_sequences and self.if_rope:
+                    hidden_states = hidden_states.flip([1])
+                    if residual is not None:
+                        residual = residual.flip([1])
+                    
+                    # rope about
+                    if self.if_rope:
+                        hidden_states = self.rope(hidden_states)
+                        if residual is not None and self.if_rope_residual:
+                            residual = self.rope(residual)
+                    
+                    if if_flip_img_sequences and self.if_rope:
+                        hidden_states = hidden_states.flip([1])
+                        if residual is not None:
+                            residual = residual.flip([1]) 
+                    
+                    hidden_states, residual = layer(
+                    hidden_states, residual, inference_params=inference_params
+                    )
+        else:
+            # get two layers in a single for-loop
+            for i in range(len(self.layers) // 2):
+                if self.if_rope:
+                    hidden_states = self.rope(hidden_states)
+                    if residual is not None and self.if_rope_residual:
+                        residual = self.rope(residual)
+
+                hidden_states_f, residual_f = self.layers[i * 2](
+                    hidden_states, residual, inference_params=inference_params
+                )
+                hidden_states_b, residual_b = self.layers[i * 2 + 1](
+                    hidden_states.flip([1]), None if residual == None else residual.flip([1]), inference_params=inference_params
+                )
+                hidden_states = hidden_states_f + hidden_states_b.flip([1])
+                residual = residual_f + residual_b.flip([1])
+        
+        
+        if not self.fused_add_norm:
+            if residual is None:
+                residual = hidden_states
+            else:
+                residual = residual + self.drop_path(hidden_states)
+            hidden_states = self.norm_f(residual.to(dtype=self.norm_f.weight.dtype))
+        else:
+            fused_add_norm_fn = rms_norm_fn if isinstance(self.norm_f, RMSNorm) else layer_norm_fn
+            hidden_states = fused_add_norm_fn(
+                self.drop_path(hidden_states),
+                self.norm_f.weight,
+                self.norm_f.bias,
+                eps=self.norm_f.eps,
+                residual=residual,
+                prenorm=False,
+                residual_in_fp32=self.residual_in_fp32,
+            )
+        
+        if self.if_cls_token:
+            return hidden_states[:, half, :] # extract CLS which is at the middle
+        else:
+            raise NotImplementedError
+             
+    
+    # def forward(self, x, extra_feat, return_features=False, inference_params=None):
+    #     x = self.forward_features(x, inference_params)
+    #     extra_feat = self.extra_feat_encoder(extra_feat)
+    #     x = torch.cat([x, extra_feat], dim=1)  # Concatenate extra features
+    #     x = self.layernorm(x)
+    #     x = self.cls_fc(x)
+    #     return x
+    
+    
+
+    def forward(self, x, extra_feat, return_features=False, inference_params=None):
+    # CLS embedding from ViM
+        cls_embed = self.forward_features(x, inference_params)          # [B, D]
+
+        # residue-level embedding
+        extra_feat = self.extra_feats_encoder(extra_feat)                   # [B, 128]
+
+        # combine CLS + residue features
+        combined = torch.cat([cls_embed, extra_feat], dim=1)             # [B, D+128]
+        combined = self.layernorm(combined)
+
+        # final logit
+        logits = self.cls_fc(combined).squeeze(-1)                      # [B]
+
+        if return_features:
+            # Return ALL embeddings for analysis
+            return logits, cls_embed, extra_feat, combined
+        else:
+            return logits
+
