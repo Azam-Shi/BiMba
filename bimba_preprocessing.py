fecaa70253d146294e8ada021056842fbb0da72b
azam shirali <ashir018@quartz.cs.fiu.edu>
2025-11-21 13:12:33 -0500

    code update

---
+++ bimba_preprocessing.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python
+
+import argparse
+from data_prepare.data_prepare import prepare
+from data_prepare.data_prepare import prepare
+
+parser = argparse.ArgumentParser(prog="PIsToN v.1.0.1")
+parser.add_argument('--config', help='config file')
+sp = parser.add_subparsers()
+
+sp_prepare = sp.add_parser('prepare', help='Data preparation module')
+sp_prepare.add_argument('--list', help='List with PPIs in format PID_A_B')
+sp_prepare.add_argument('--ppi', help='PPI in format PID_A_B (mutually exclusive with the --list option)')
+sp_prepare.add_argument('--no_download',  default=False, action="store_true", help='If set True, the pipeline will skip the download part.')
+sp_prepare.add_argument('--download_only',  default=False, action="store_true", help='If set True, the program will only download PDB structures without processing them.')
+
+sp_prepare.add_argument('--fix_pdb', default=False, action='store_true', help='If set True, fix PDB structures.')
+
+sp_prepare.set_defaults(func=prepare)
+args = parser.parse_args()
+
+if vars(args).get('func') is None:
+    print("Error: Please provide arguments (see the help message below).")
+    parser.print_help()
+    exit()
+
+args.func(args)
+
+
+
