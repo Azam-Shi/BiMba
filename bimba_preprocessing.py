ece1b8caad0cc8888e03c7c97da2018d055e46e9
Azam Shirali <105470322+Azam-Shi@users.noreply.github.com>
2026-02-01 20:32:10 -0500

    Update bimba_preprocessing.py

--- bimba_preprocessing.py
+++ bimba_preprocessing.py
@@ -4,13 +4,13 @@ import argparse
 from data_prepare.data_prepare import prepare
 from data_prepare.data_prepare import prepare
 
-parser = argparse.ArgumentParser(prog="PIsToN v.1.0.1")
+parser = argparse.ArgumentParser(prog="BiMba")
 parser.add_argument('--config', help='config file')
 sp = parser.add_subparsers()
 
 sp_prepare = sp.add_parser('prepare', help='Data preparation module')
-sp_prepare.add_argument('--list', help='List with PPIs in format PID_A_B')
-sp_prepare.add_argument('--ppi', help='PPI in format PID_A_B (mutually exclusive with the --list option)')
+sp_prepare.add_argument('--list', help='List with proteins in format PID_A or protein complexes in format PID_A_B')
+sp_prepare.add_argument('--p', help='proteins in format PID_A or PID_A_B (mutually exclusive with the --list option)')
 sp_prepare.add_argument('--no_download',  default=False, action="store_true", help='If set True, the pipeline will skip the download part.')
 sp_prepare.add_argument('--download_only',  default=False, action="store_true", help='If set True, the program will only download PDB structures without processing them.')
 
